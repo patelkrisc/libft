f0461e61442de847e702fde9faddf021f726508b
kpatel <patelkrisc@gmail.com>
2017-11-17 17:49:41 -0800

    reupload

---
+++ ft_strtrim.c
@@ -0,0 +1,31 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strtrim.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: kpatel <patelkrisc@gmail.com>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2017/10/02 16:23:04 by kpatel            #+#    #+#             */
+/*   Updated: 2017/10/12 19:50:06 by kpatel           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+char	*ft_strtrim(char const *s)
+{
+	size_t	i;
+	size_t	j;
+
+	i = 0;
+	if (!s)
+		return (0);
+	while (s[i] == ' ' || s[i] == '\n' || s[i] == '\t')
+		i++;
+	if (s[i] == '\0')
+		return (ft_strdup(s + i));
+	j = ft_strlen(s) - 1;
+	while (s[j] == ' ' || s[j] == '\n' || s[j] == '\t')
+		j--;
+	return (ft_strsub(s, i, j - i + 1));
+}
